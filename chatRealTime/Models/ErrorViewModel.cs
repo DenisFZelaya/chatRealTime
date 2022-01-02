926585ba82540a3376468fc06d5068aa9e4be483
Denis Federico Zelaya <Denisfzelaya@gmail.com>
2022-01-02 16:54:56 -0600

    Initial commit

---
+++ chatRealTime/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace chatRealTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
